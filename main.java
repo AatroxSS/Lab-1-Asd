04b09dee6ad76c36cb12c240ad0208b1d1b21f89
AatroxSS <arsenijruslanovic@gmail.com>
2026-04-27 00:16:53 +0300

    Implement main class for QueueArray demonstration
    Added a main class to demonstrate QueueArray functionality.

--- main.java
+++ main.java
@@ -1,1 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        QueueArray queue = new QueueArray(5);
 
+        queue.enqueue(10.5);
+        queue.enqueue(20.3);
+        queue.enqueue(30.7);
+        queue.enqueue(40.1);
+        queue.enqueue(50.9);
+
+        queue.enqueue(99.9);
+
+        queue.display();
+
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.display();
+    }
+}
